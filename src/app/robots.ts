7e0e6e8648e894f8d6fb4fddf3a13d78c6c4bf37
ErwannRousseau <erwann.rousseau@icloud.com>
2026-02-08 17:12:05 +0100

    feat: add robots

---
+++ src/app/robots.ts
@@ -0,0 +1,13 @@
+import type { MetadataRoute } from "next";
+import { env } from "~/env";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      allow: "/",
+      disallow: "/studio/",
+      userAgent: "*",
+    },
+    sitemap: `${env.NEXT_PUBLIC_SITE_URL}/sitemap.xml`,
+  };
+}
